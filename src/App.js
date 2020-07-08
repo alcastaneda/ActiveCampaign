1ef4ec98fdd643ad1d68a0f38b310a117006d37d
Adriana <adriana.lcs316@gmail.com>
2020-07-08 00:30:43 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.scss";
+
+import React from "react";
+import DonationForm from "./components/DonationForm/DonationForm";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <DonationForm />
+    </div>
+  );
+};
+
+export default App;
